c543130a8a090c38288028220ed45dcaff7e3daf
noituri <mikolaj.radkowski@protonmail.com>
2024-08-16 16:56:53 +0200

    fix: glsl lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,149 +1,152 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "zls",
-                    "rust_analyzer",
-                    "html",
-                    "cssls",
-                    "yamlls",
-                    "wgsl_analyzer",
-                    -- "glslls",
-                    "jsonls",
-                    "dockerls",
-                    "bashls",
-                    "marksman",
-                    "tsserver",
-                    "clangd",
-                    "cmake",
-                    "gopls",
-                },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.zls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.rust_analyzer.setup({
-                capabilities = capabilities,
-                settings = {
-                    ["rust-analyzer"] = {},
-                },
-            })
-            lspconfig.html.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.cssls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.yamlls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.wgsl_analyzer.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.jsonls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.dockerls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.bashls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.marksman.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.tsserver.setup({
-                capabilities = capabilities,
-            })
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"zls",
+					"rust_analyzer",
+					"html",
+					"cssls",
+					"yamlls",
+					"wgsl_analyzer",
+					"glslls",
+					"jsonls",
+					"dockerls",
+					"bashls",
+					"marksman",
+					"tsserver",
+					"clangd",
+					"cmake",
+					"gopls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.zls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+				settings = {
+					["rust-analyzer"] = {},
+				},
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.yamlls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.wgsl_analyzer.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jsonls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.dockerls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.marksman.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.glslls.setup({
+				capabilities = capabilities,
+			})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-            vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    {
-        "L3MON4D3/LuaSnip",
-        dependencies = {
-            "rafamadriz/friendly-snippets",
-            "saadparwaiz1/cmp_luasnip",
-        },
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        config = function()
-            local cmp = require("cmp")
-            require("luasnip.loaders.from_vscode").lazy_load()
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			"saadparwaiz1/cmp_luasnip",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
 
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                window = {
-                    completion = {
-                        border = "rounded",
-                        winhighlight = "Normal:bg0,FloatBorder:bg0",
-                    },
-                    documentation = {
-                        border = "rounded",
-                        winhighlight = "Normal:bg0,FloatBorder:bg0",
-                    },
-                },
-                mapping = {
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.close(),
-                    ["<CR>"] = cmp.mapping.confirm({
-                        behavior = cmp.ConfirmBehavior.Insert,
-                        select = true,
-                    }),
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        else
-                            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-                        end
-                    end, { "i", "s" }),
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = {
+						border = "rounded",
+						winhighlight = "Normal:bg0,FloatBorder:bg0",
+					},
+					documentation = {
+						border = "rounded",
+						winhighlight = "Normal:bg0,FloatBorder:bg0",
+					},
+				},
+				mapping = {
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.close(),
+					["<CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Insert,
+						select = true,
+					}),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+						end
+					end, { "i", "s" }),
 
-                    ["<S-Tab>"] = cmp.mapping(function()
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-                            feedkey("<Plug>(vsnip-jump-prev)", "")
-                        end
-                    end, { "i", "s" }),
-                },
-                sources = {
-                    { name = "nvim_lsp" },
-                    { name = "lunasnip" },
-                    { name = "buffer" }
-                },
-            })
-        end,
-    },
+					["<S-Tab>"] = cmp.mapping(function()
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+							feedkey("<Plug>(vsnip-jump-prev)", "")
+						end
+					end, { "i", "s" }),
+				},
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "lunasnip" },
+					{ name = "buffer" },
+				},
+			})
+		end,
+	},
 }
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,48 +1,51 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        vim.filetype.add({ extension = { wgsl = "wgsl" } })
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		vim.filetype.add({ extension = { wgsl = "wgsl" } })
 
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            auto_install = true,
-            ensure_installed = {
-                "lua",
-                "rust",
-                "c",
-                "cpp",
-                "cmake",
-                "css",
-                "csv",
-                "dart",
-                "diff",
-                "dockerfile",
-                "elixir",
-                "erlang",
-                "gitignore",
-                "glsl",
-                "go",
-                "html",
-                "java",
-                "javascript",
-                "json",
-                "latex",
-                "llvm",
-                "make",
-                "python",
-                "sql",
-                "svelte",
-                "tsx",
-                "toml",
-                "typescript",
-                "wgsl",
-                "zig",
-                "yaml",
-                "nix",
-            },
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			auto_install = true,
+			ensure_installed = {
+				"lua",
+				"rust",
+				"c",
+				"cpp",
+				"cmake",
+				"css",
+				"csv",
+				"dart",
+				"diff",
+				"dockerfile",
+				"elixir",
+				"erlang",
+				"gitignore",
+				"glsl",
+				"go",
+				"html",
+				"java",
+				"javascript",
+				"json",
+				"latex",
+				"llvm",
+				"make",
+				"python",
+				"sql",
+				"svelte",
+				"tsx",
+				"toml",
+				"typescript",
+				"wgsl",
+				"zig",
+				"yaml",
+				"nix",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+
+		vim.filetype.add({ extension = { vert = "glsl" } })
+		vim.filetype.add({ extension = { frag = "glsl" } })
+	end,
 }
